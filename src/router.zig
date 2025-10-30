eb033fb0528d9adc5a830dc239458afd02807269
Haroun Moatassime <haroun.moatassime@gmail.com>
2025-10-30 21:57:31 +0100

    add route matching parameter

--- src/router.zig
+++ src/router.zig
@@ -5,15 +5,21 @@ pub const Route = struct {
     method: http.Method,
     path: []const u8,
     handler: *const fn (std.mem.Allocator, *http.Server.Request) anyerror!void,
+    match: enum { exact, prefix },
 };
 
 pub fn Router(comptime routes: []const Route) type {
     return struct {
         pub fn route(allocator: std.mem.Allocator, request: *http.Server.Request) !void {
             inline for (routes) |r| {
-                if (r.method == request.head.method and
-                    std.mem.eql(u8, r.path, request.head.target))
-                {
+                if (r.method != request.head.method) continue;
+
+                const matches = switch (r.match) {
+                    .exact => std.mem.eql(u8, r.path, request.head.target),
+                    .prefix => std.mem.eql(u8, r.path, request.head.target),
+                };
+
+                if (matches) {
                     return r.handler(allocator, request);
                 }
             }
